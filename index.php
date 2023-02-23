d4c102a9d2e7802a5966326ecf4ba31158dbb68e
Fufroma <fufroma@gmail.com>
2023-02-23 20:23:14 +0100

    Refonte complète du player Radio Dogmazic
    Dans le désordre :
    - plus de player externe, juste le player HTML5 des navigateurs
    - gestion intelligente du refresh (plus tout les 5 secondes quoi qu'il arrive)
    - resize des images
    - nouvelle methode pour la récupération des metadata
      - ne se base plus sur les informations de Icecast
      - récupére les informations exactes du titre
      - donne des liens directs vers le titre / l'artiste / l'album
      - on a la jaquette \o/

--- index.php
+++ index.php
@@ -1,45 +1,145 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://play.dogmazic.net/");
+$flux = 'https://radio.dogmazic.net:8001/stream.mp3';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <title>Radio Dogmazic</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="team" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-     <title>Radio Dogmazic</title>
-<!--
+  <script src="/jquery-3.6.3.min.js" ></script>
 
--->
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-     <meta name="description" content="">
-     <meta name="keywords" content="">
-     <meta name="team" content="">
-     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-<style>
- body {
+  <style>
+    body {
       background-color: #91949B;
+      background-color: #222;
+      color: #ff9d00;
+    }
 
-</style>
+    .center {
+      text-align:center;
+      width:100%;
+    }
+
+    a {
+      color: #ff9d00;
+    }
+  </style>
+</head>
 
 <body>
-<center>
-<img src="Logo-DGZ-TRANSPARENT.png" alt="dogmazic radio" width=400/></center><center> <!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
-<script type="text/javascript" src="https://hosted.muses.org/mrp.js"></script>
-<script type="text/javascript">
-MRP.insert({
-'url':'/stream.mp3',
-'codec':'mp3',
-'volume':60,
-'autoplay':true,
-'forceHTML5':true,
-'jsevents':true,
-'buffering':0,
-'title':'Radio Dogmazic',
-'wmode':'transparent',
-'skin':'abrahadabra2',
-'width':100,
-'height':141
-});</script><!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE --> </center><center><object data="/nowplaying.xsl" type="text/html" width="467" height="25"></object></center> </center>
+
+<div class="center">
+
+  <img src="/Logo-DGZ-TRANSPARENT.png" alt="Dogmazic Logo" width="400" height="515">
+
+  <br>
+  <br>
+
+  <audio controls id="dogplayer" onpause="refreshInfos()" onplay="refreshInfos()" >
+    <source src="<?php echo $flux; ?>" type="audio/mpeg">
+    <p>
+      Look like your browser can't handle HTML5, here the <a href="<?php echo $flux; ?>">direct link</a>.
+    </p>
+  </audio>
+
+  <br>
+  <br>
+
+  <div id="display_infos">
+    <span id="metainfos">
+      <a href='#' title='Show this artist on Dogmazic' id="link_artist" target=_blank ></a> -
+      <a href='#' title='Show this song on Dogmazic' id="link_song" target=_blank ></a>
+    </span>
+
+    <br>
+    <br>
+
+    <a href="#" target=_blank id="link_album">
+      <img src='/blank_album_art.png' alt="Album Art" title="Show this album on Dogmazic" id="albumart" width="125" height="125">
+      <br>
+      <span id="album_title"></span>
+    </a>
+  </div>
+
+  <img src='/pause.png' alt="Pause" title="Paused" id="pauseimg" width="125" height="125" onclick="playRadio()">
+
+</div>
+
+
+<script>
+
+function playRadio() {
+  document.getElementById('dogplayer').play();
+}
+
+// Need this as a global var for refreshInfos()
+var current_song_id = null;
+
+function refreshInfos() {
+  // No refresh if the page isn't visible
+  if (document.hidden) {
+    return;
+  }
+
+  // No refresh if the player is paused
+  if ( document.getElementById('dogplayer').paused ) {
+    $("#display_infos").hide();
+    $("#pauseimg").show();
+    return;
+  }
+
+  // Ok, get the refresh infos
+  $.get("/metadata.php?wanted=json", function( data ) {
+    var obj = jQuery.parseJSON( data );
+
+    // If we already set this song infos, quit
+    if ( current_song_id == obj['title_id'] ) {
+      return;
+    }
+    current_song_id = obj['title_id'];
+
+    // Set all the informations
+    $("#album_title").html( obj['album']);
+    $("#albumart").attr('src', obj['label_img'] );
+    $("#link_album").attr('href', obj['album_url'] );
+    $("#link_artist").attr('href', obj['artist_url']);
+    $("#link_artist").html(obj['artist']);
+    $("#link_song").attr('href', obj['song_url']);
+    $("#link_song").html(obj['title']);
+
+    // And display them
+    $("#pauseimg").hide();
+    $("#display_infos").show();
+  });
+}
+
+
+// ---- REFRESH INFOS, when?
+// at page load...
+refreshInfos();
+
+// refresh every X milliseconds
+setInterval(function(){
+  refreshInfos()
+}, 5000); // 5 seconds
+
+// and when we display the page (ex: switching tabs)
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "visible") {
+    refreshInfos();
+  }
+});
+
+// ---- END REFRESH INFOS
+
+</script>
+
 </body>
 </html>
---
+++ metadata.php
@@ -0,0 +1,95 @@
+<?php
+
+include_once('conf.inc.php');
+
+// Ce script retourne le fichier exact en cours de lecture par Ezstream.
+// Il utilise lsof :
+// $ lsof -c ezstream -Fn|grep '^n/var' | sed -e 's/^n//g'
+exec('/usr/bin/sudo /etc/ezstream/get_running_ziq.sh', $output, $rcode);
+
+// Pas de fichier, ezstream ne tourne peut-être pas.
+if (empty($output)) die('No music');
+
+$ziq = $output[0];
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Database error");
+
+$query = "
+select 
+  a.name as artist, 
+  a.id as artist_id, 
+  al.name as album, 
+  al.id as album_id, 
+  s.title, 
+  s.id as title_id 
+from 
+  song s, 
+  artist a, 
+  album al 
+where 
+  a.id = s.artist 
+  and al.id = s.album 
+  and s.file='". $db->real_escape_string( $ziq  )  ."' 
+limit 1;";
+
+$result = $db->query($query);
+
+$obj = $result->fetch_object() ;
+
+if (is_null($obj)) die();
+
+if (isset($_GET['wanted'])) {
+  $wanted=strtolower(trim($_GET['wanted']));
+}
+
+// Add some usefull properties
+$obj->artist_url="https://play.dogmazic.net/artists.php?action=show&artist=".$obj->artist_id;
+$obj->album_url="https://play.dogmazic.net/albums.php?action=show&album=".$obj->album_id;
+$obj->song_url="https://play.dogmazic.net/song.php?action=show_song&song_id=".$obj->title_id;
+$obj->label_img = "https://play.dogmazic.net/image.php?object_id=". $obj->album_id  ."&object_type=album&thumb=125";
+
+// If you want to redirect somewhere...
+function go($url){
+  header('Location: '.$url);
+  exit();
+}
+
+switch($wanted){
+  case 'img':
+    echo $obj->label_img;
+    break;
+  case 'artist':
+    echo $obj->artist;
+    break;
+  case 'artist_url':
+    echo $obj->artist_url;
+    break;
+  case 'artist_go':
+    go($obj->artist_url);
+    break;
+  case 'album':
+    echo $obj->album;
+    break;
+  case 'album_url':
+    echo $obj->album_url;
+    break;
+  case 'album_go':
+    go($obj->album_url);
+    break;
+  case 'song':
+    echo $obj->title;
+    break;
+  case 'song_url':
+    echo $obj->song_url;
+    break;
+  case 'song_go':
+    go($obj->song_url);
+    break;
+  case 'json':
+    echo json_encode($obj);
+    break;
+  default:
+    echo $obj->artist." - ".$obj->album." - ".$obj->title;
+    break;
+}
+
