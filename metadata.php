f07efc4b43df2cb3371b77306de3b6120ff16a17
Alan <fufroma@gmail.com>
2025-06-15 21:45:33 +0200

    CS

--- metadata.php
+++ metadata.php
@@ -10,23 +10,25 @@ $m = new Memcached();
 $m->addServer('localhost', 11211);
 
 $obj = $m->get('metadata');
-if ( !$obj || empty( $obj ) ){
-  // Ce script retourne le fichier exact en cours de lecture par Ezstream.
-  // Il utilise lsof :
-  // $ lsof -c ezstream -Fn|grep '^n/var' | sed -e 's/^n//g'
-  #exec('/usr/bin/sudo /etc/ezstream/get_running_ziq.sh', $output, $rcode);
-  // En compilant le script avec shc et en lui mettant un setuid
-  // cela permet de l'executer sans sudo, ce qui allége les logs
-  exec('/etc/ezstream/get_running_ziq.sh.x', $output, $rcode);
+if (!$obj || empty($obj)) {
+    // Ce script retourne le fichier exact en cours de lecture par Ezstream.
+    // Il utilise lsof :
+    // $ lsof -c ezstream -Fn|grep '^n/var' | sed -e 's/^n//g'
+    #exec('/usr/bin/sudo /etc/ezstream/get_running_ziq.sh', $output, $rcode);
+    // En compilant le script avec shc et en lui mettant un setuid
+    // cela permet de l'executer sans sudo, ce qui allége les logs
+    exec('/etc/ezstream/get_running_ziq.sh.x', $output, $rcode);
 
-  // Pas de fichier, ezstream ne tourne peut-être pas.
-  if (empty($output)) die('No music');
+    // Pas de fichier, ezstream ne tourne peut-être pas.
+    if (empty($output)) {
+        die('No music');
+    }
 
-  $ziq = $output[0];
+    $ziq = $output[0];
 
-  $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Database error");
+    $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Database error");
 
-  $query = "
+    $query = "
   select
     a.name as artist,
     a.id as artist_id,
@@ -41,83 +43,85 @@ if ( !$obj || empty( $obj ) ){
   where
     a.id = s.artist
     and al.id = s.album
-    and s.file='". $db->real_escape_string( $ziq  )  ."'
+    and s.file='" . $db->real_escape_string($ziq) . "'
   limit 1;";
 
-  $result = $db->query($query);
+    $result = $db->query($query);
 
-  $obj = $result->fetch_object() ;
+    $obj = $result->fetch_object() ;
 
-  if (is_null($obj)) die();
+    if (is_null($obj)) {
+        die();
+    }
 
-  $m->set('metadata', $obj, CACHE_IN_SECONDS);
+    $m->set('metadata', $obj, CACHE_IN_SECONDS);
 
-  $db->close();
+    $db->close();
 }
 
 
 if (isset($_GET['wanted'])) {
-  $wanted=strtolower(trim($_GET['wanted']));
-} else if ( $argc == 2)  {
-  $wanted=trim(strtolower($argv[1]));
+    $wanted=strtolower(trim($_GET['wanted']));
+} elseif ($argc == 2) {
+    $wanted=trim(strtolower($argv[1]));
 } else {
-  $wanted='default';
+    $wanted='default';
 }
 
 // Add some usefull properties
-$obj->artist_url="https://play.dogmazic.net/artists.php?action=show&artist=".$obj->artist_id;
-$obj->album_url="https://play.dogmazic.net/albums.php?action=show&album=".$obj->album_id;
-$obj->song_url="https://play.dogmazic.net/song.php?action=show_song&song_id=".$obj->title_id;
-$obj->label_img = "https://play.dogmazic.net/image.php?object_id=". $obj->album_id  ."&object_type=album&thumb=125";
+$obj->artist_url="https://play.dogmazic.net/artists.php?action=show&artist=" . $obj->artist_id;
+$obj->album_url ="https://play.dogmazic.net/albums.php?action=show&album=" . $obj->album_id;
+$obj->song_url  ="https://play.dogmazic.net/song.php?action=show_song&song_id=" . $obj->title_id;
+$obj->label_img = "https://play.dogmazic.net/image.php?object_id=" . $obj->album_id . "&object_type=album&thumb=125";
 
 // If you want to redirect somewhere...
-function go($url){
-  header('Location: '.$url);
-  exit();
-}
-
-switch($wanted){
-  case 'img':
-    echo $obj->label_img;
-    break;
-  case 'artist':
-    echo $obj->artist;
-    break;
-  case 'artist_url':
-    echo $obj->artist_url;
-    break;
-  case 'artist_go':
-    go($obj->artist_url);
-    break;
-  case 'album':
-    echo $obj->album;
-    break;
-  case 'album_url':
-    echo $obj->album_url;
-    break;
-  case 'album_go':
-    go($obj->album_url);
-    break;
-  case 'title':
-  case 'song':
-    echo $obj->title;
-    break;
-  case 'title_url':
-  case 'song_url':
-    echo $obj->song_url;
-    break;
-  case 'title_go':
-  case 'song_go':
-    go($obj->song_url);
-    break;
-  case 'json':
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($obj);
+function go($url)
+{
+    header('Location: ' . $url);
     exit();
-    break;
-  default:
-    #echo $obj->artist." - ".$obj->album." - ".$obj->title;
-    echo $obj->artist." - ".$obj->title;
-    break;
 }
 
+switch ($wanted) {
+    case 'img':
+        echo $obj->label_img;
+        break;
+    case 'artist':
+        echo $obj->artist;
+        break;
+    case 'artist_url':
+        echo $obj->artist_url;
+        break;
+    case 'artist_go':
+        go($obj->artist_url);
+        break;
+    case 'album':
+        echo $obj->album;
+        break;
+    case 'album_url':
+        echo $obj->album_url;
+        break;
+    case 'album_go':
+        go($obj->album_url);
+        break;
+    case 'title':
+    case 'song':
+        echo $obj->title;
+        break;
+    case 'title_url':
+    case 'song_url':
+        echo $obj->song_url;
+        break;
+    case 'title_go':
+    case 'song_go':
+        go($obj->song_url);
+        break;
+    case 'json':
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($obj);
+        exit();
+        break;
+    default:
+        #echo $obj->artist." - ".$obj->album." - ".$obj->title;
+        echo $obj->artist . " - " . $obj->title;
+        break;
+}
--- metadata_of_song.php
+++ metadata_of_song.php
@@ -6,20 +6,22 @@ include_once('conf.inc.php');
 header('Access-Control-Allow-Origin: *');
 
 $song_id=intval($_REQUEST['song_id']);
-if ($song_id == 0 ) die('No song id');
+if ($song_id == 0) {
+    die('No song id');
+}
 
 // Léger cache Memcached
 $m = new Memcached();
 $m->addServer('localhost', 11211);
 
-$metadata_keyname = 'metadata_song_'.intval($song_id);
+$metadata_keyname = 'metadata_song_' . intval($song_id);
 
 $obj = $m->get($metadata_keyname);
-if ( !$obj || empty( $obj ) ){
+if (!$obj || empty($obj)) {
 
-  $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Database error");
+    $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Database error");
 
-  $query = "
+    $query = "
   select
     a.name as artist,
     a.id as artist_id,
@@ -34,83 +36,85 @@ if ( !$obj || empty( $obj ) ){
   where
     a.id = s.artist
     and al.id = s.album
-    and s.id='". $db->real_escape_string( $song_id  )  ."'
+    and s.id='" . $db->real_escape_string($song_id) . "'
   limit 1;";
 
-  $result = $db->query($query);
+    $result = $db->query($query);
 
-  $obj = $result->fetch_object() ;
+    $obj = $result->fetch_object() ;
 
-  if (is_null($obj)) die();
+    if (is_null($obj)) {
+        die();
+    }
 
-  $m->set($metadata_keyname, $obj, CACHE_IN_SECONDS);
+    $m->set($metadata_keyname, $obj, CACHE_IN_SECONDS);
 
-  $db->close();
+    $db->close();
 }
 
 
 if (isset($_GET['wanted'])) {
-  $wanted=strtolower(trim($_GET['wanted']));
-} else if ( $argc == 2)  {
-  $wanted=trim(strtolower($argv[1]));
+    $wanted=strtolower(trim($_GET['wanted']));
+} elseif ($argc == 2) {
+    $wanted=trim(strtolower($argv[1]));
 } else {
-  $wanted='default';
+    $wanted='default';
 }
 
 // Add some usefull properties
-$obj->artist_url="https://play.dogmazic.net/artists.php?action=show&artist=".$obj->artist_id;
-$obj->album_url="https://play.dogmazic.net/albums.php?action=show&album=".$obj->album_id;
-$obj->song_url="https://play.dogmazic.net/song.php?action=show_song&song_id=".$obj->title_id;
-$obj->label_img = "https://play.dogmazic.net/image.php?object_id=". $obj->album_id  ."&object_type=album&thumb=125";
+$obj->artist_url="https://play.dogmazic.net/artists.php?action=show&artist=" . $obj->artist_id;
+$obj->album_url ="https://play.dogmazic.net/albums.php?action=show&album=" . $obj->album_id;
+$obj->song_url  ="https://play.dogmazic.net/song.php?action=show_song&song_id=" . $obj->title_id;
+$obj->label_img = "https://play.dogmazic.net/image.php?object_id=" . $obj->album_id . "&object_type=album&thumb=125";
 
 // If you want to redirect somewhere...
-function go($url){
-  header('Location: '.$url);
-  exit();
-}
-
-switch($wanted){
-  case 'img':
-    echo $obj->label_img;
-    break;
-  case 'artist':
-    echo $obj->artist;
-    break;
-  case 'artist_url':
-    echo $obj->artist_url;
-    break;
-  case 'artist_go':
-    go($obj->artist_url);
-    break;
-  case 'album':
-    echo $obj->album;
-    break;
-  case 'album_url':
-    echo $obj->album_url;
-    break;
-  case 'album_go':
-    go($obj->album_url);
-    break;
-  case 'title':
-  case 'song':
-    echo $obj->title;
-    break;
-  case 'title_url':
-  case 'song_url':
-    echo $obj->song_url;
-    break;
-  case 'title_go':
-  case 'song_go':
-    go($obj->song_url);
-    break;
-  case 'json':
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($obj);
+function go($url)
+{
+    header('Location: ' . $url);
     exit();
-    break;
-  default:
-    #echo $obj->artist." - ".$obj->album." - ".$obj->title;
-    echo $obj->artist." - ".$obj->title;
-    break;
 }
 
+switch ($wanted) {
+    case 'img':
+        echo $obj->label_img;
+        break;
+    case 'artist':
+        echo $obj->artist;
+        break;
+    case 'artist_url':
+        echo $obj->artist_url;
+        break;
+    case 'artist_go':
+        go($obj->artist_url);
+        break;
+    case 'album':
+        echo $obj->album;
+        break;
+    case 'album_url':
+        echo $obj->album_url;
+        break;
+    case 'album_go':
+        go($obj->album_url);
+        break;
+    case 'title':
+    case 'song':
+        echo $obj->title;
+        break;
+    case 'title_url':
+    case 'song_url':
+        echo $obj->song_url;
+        break;
+    case 'title_go':
+    case 'song_go':
+        go($obj->song_url);
+        break;
+    case 'json':
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($obj);
+        exit();
+        break;
+    default:
+        #echo $obj->artist." - ".$obj->album." - ".$obj->title;
+        echo $obj->artist . " - " . $obj->title;
+        break;
+}
